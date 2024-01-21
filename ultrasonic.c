ebdaf4678e7eca9a387c66e35f0c6f88f78b0308
Christian Canelea <127325638+ChrisCanelea@users.noreply.github.com>
2024-01-20 21:38:58 -0500

    pure ultson code (w/ error correction)

---
+++ ultrasonic.c
@@ -0,0 +1,54 @@
+/*
+ * created by Rui Santos, https://randomnerdtutorials.com
+ * 
+ * Complete Guide for Ultrasonic Sensor HC-SR04
+ *
+    Ultrasonic sensor Pins:
+        VCC: +5VDC
+        Trig : Trigger (INPUT) - Pin11
+        Echo: Echo (OUTPUT) - Pin 12
+        GND: GND
+ */
+ 
+int trigPin = 11;    // Trigger
+int echoPin = 12;    // Echo
+double duration, cm, prev_cm;
+ 
+void setup() {
+  //Serial Port begin
+  Serial.begin (9600);
+  //Define inputs and outputs
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  prev_cm = 150;
+}
+ 
+void loop() {
+  // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+ 
+  // Read the signal from the sensor: a HIGH pulse whose
+  // duration is the time (in microseconds) from the sending
+  // of the ping to the reception of its echo off of an object.
+  duration = pulseIn(echoPin, HIGH);
+  
+  // Convert the time into a distance
+  cm = ((double)duration/2.0) / 29.1;     // Divide by 29.1 or multiply by 0.0343
+
+  if (cm > 200) 
+  {
+    cm = prev_cm;
+  }  else 
+  {
+    prev_cm = cm;
+  }
+  
+  Serial.println(cm);
+    
+  delay(150);
+}
